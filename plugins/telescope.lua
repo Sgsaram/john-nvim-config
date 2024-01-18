ec5258805c5ed9fcfdb541602556454e14059b2a
john <john-mail>
2024-01-19 00:07:44 +0300

    Changed something

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -4,10 +4,21 @@ return {
 		"nvim-lua/plenary.nvim"
 	},
 	config = function()
+		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
+		local actions = require("telescope.actions")
+		telescope.setup({
+			defaults = {
+				path_display = { "truncate " },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+					},
+				},
+			},
+		})
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
-		
-		local telescope = require("telescope")
 	end
 }
